79e314378332ac2197aba77c04aef1997c995a56
Jyotiraditya <jyotiradityaraghuvanshi75@gmail.com>
2024-09-28 17:53:10 +0530

    another commit

--- src/OOPs/Abstraction/LearnAnonymousClass.java
+++ src/OOPs/Abstraction/LearnAnonymousClass.java
@@ -12,6 +12,7 @@ public class LearnAnonymousClass {
         obj.outermethod();
         obj.getClass();
 
+        // we are creating a child class by extending above and implementing below using anonymous class.
         OuterInter obj2 = new OuterInter() { // It seems like it is an interface object but that can't possible this is child class object.
             @Override
             public void vegetable() {
@@ -19,8 +20,29 @@ public class LearnAnonymousClass {
                 System.out.println("This method has bunch of vegetables");
             }
         };
+
+        obj2.vegetable();   // This vegetable function can also be made using lambda expression.
+        OuterInter obj3 =  () -> {
+            System.out.println("Interface method is having its new body");
+            System.out.println("This is achieved using lambda expression");
+        };
+
+        obj3.vegetable(); // Lambda expression can only be used if there is only single method in interface i.e. it is a FunctionalInterface.
+
+        funcInter lambda = (x , y) -> { // this is lambda expression where we do not need to make anonymous class instead we just write a short code.
+            System.out.println("Adding the values using lambda expression");
+            return x + y;
+        }  ;
+        int val = lambda.adding(236283 , 37281486);
+        System.out.println(val);
+
     }
 
+}
+
+@FunctionalInterface
+interface funcInter{ // How can we achieve the calling of this method in functional interface ? using Lambda Expression.
+    int adding(int x , int y);
 }
 
 class OuterClass{
@@ -30,6 +52,7 @@ class OuterClass{
     }
 }
 
-interface OuterInter{
+@FunctionalInterface
+interface OuterInter{  // This is a functional interface -> interfaces which have single method in it are called fun. interface
     void vegetable();
 }
